caf27ddc2447bac14cb2d4c11ad78641d73985b0
Paulius Balčiūnas <49620811+pauliusB20@users.noreply.github.com>
2023-08-01 21:32:54 +0300

    fixes

---
+++ FallDown/Assets/Scripts/ObstacleMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] bool changeDirection = false;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        var direction = changeDirection ? 1 : -1;
+
+        transform.position += (new Vector3(moveSpeed, 0f, 0f) * Time.deltaTime * direction);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "obstacleStatic") {
+            changeDirection = !changeDirection;
+        }
+    }
+}
